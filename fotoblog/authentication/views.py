06f5edf8fbf10f6e3c01cbfb06ea26a068e8bd37
souiai <sou.dali@gmail.com>
2023-09-06 16:43:31 +0200

    revision cour django

--- fotoblog/authentication/views.py
+++ fotoblog/authentication/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from . import forms
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+
+def login_page(request):
+    form = forms.LoginForm()
+    message = ''
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['passwoed']
+            )
+            if user is not None:
+                login(request, user)
+                message = f'Bonjour, {user.username}! vous êtes connecté.'
+            else:
+                message = 'identifiant invalide.'
+    return render(
+        request,
+        'authentcation/login.html',
+        context={'message': message, 'form': form}
+    )
