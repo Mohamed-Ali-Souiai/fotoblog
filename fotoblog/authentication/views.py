ec23066a2c256d54c14ebd6432a744e4d5da6218
souiai <sou.dali@gmail.com>
2023-09-08 13:58:49 +0200

    revision cour django

--- fotoblog/authentication/views.py
+++ fotoblog/authentication/views.py
@@ -1,9 +1,68 @@
 from django.shortcuts import render, redirect
+from django.conf import settings
 from . import forms
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View
 
 
-def login_page(request):
+class SignupPage(View):
+    form_class = forms.SignupForm
+    template_name = 'authentication/signup.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, context={'form': form})
+
+    def post(self,request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+        return render(request, self.template_name, context={'form': form})
+
+
+class LoginPageView(View,LoginRequiredMixin):
+    form_class = forms.LoginForm
+    template_name = 'authentication/login.html'
+
+    def get(self, request):
+        form = self.form_class()
+        message = ''
+        return render(
+            request,
+            self.template_name,
+            context={'message': message, 'form': form}
+        )
+
+    def post(self, request):
+        message = ''
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password']
+            )
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+                # message = f'Bonjour, {user.username}! vous êtes connecté.'
+            else:
+                message = 'identifiant invalide.'
+        return render(
+            request,
+            self.template_name,
+            context={'message': message, 'form': form}
+        )
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login_page')
+
+
+"""def login_page(request):
     form = forms.LoginForm()
     message = ''
     if request.method == 'POST':
@@ -23,9 +82,4 @@ def login_page(request):
         request,
         'authentication/login.html',
         context={'message': message, 'form': form}
-    )
-
-
-def logout_user(request):
-    logout(request)
-    return redirect('login_page')
+    )"""
